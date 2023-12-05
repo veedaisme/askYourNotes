0395a4947f99eb83c78ccd8d495da2b37123bc8c
Iqbal Fakhri <iqbal.fakhri@tech.jago.com>
2023-12-05 16:59:11 +0700

    test

--- src/vectorDb/index.ts
+++ src/vectorDb/index.ts
@@ -4,6 +4,7 @@ import VectorDbClient from "./VectorDb";
 const vectorDbBaseUrl = config.chromaDbBaseUrl;
 
 console.info(`[INFO] Connecting to vector db at ${vectorDbBaseUrl}`)
+console.info(`[INFO] openai api key ${config.openAIKey}`)
 
 const vectorDbClient = new VectorDbClient({
   embedderApiKey: config.openAIKey,
